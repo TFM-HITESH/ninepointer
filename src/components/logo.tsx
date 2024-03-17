a80512fdf86fca1edf323c9e2ac6153b88b0fb8e
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-03-18 03:02:26 +0530

    improvement: Logo changes

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <div>
-      <Image src="" alt="NinePointer" width={140} height={140} />
+      <Image src="/ninelogo.svg" alt="NinePointer" width={60} height={60} />
     </div>
   );
 };
