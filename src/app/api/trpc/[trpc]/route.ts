a01856250f6ef879aea8f4e74c21062398da3bd0
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-03-19 02:18:23 +0530

    temp: Temporary Deployment Bypass

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,6 +1,6 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from '@/server/api/root';
-export const maxDuration = 300;
+export const maxDuration = 9;
 
 const handler = (req: Request) =>
   fetchRequestHandler({
